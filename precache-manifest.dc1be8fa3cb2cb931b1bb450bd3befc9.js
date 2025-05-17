3c6be23ba7c2fe8dd773736c0feebb5a9e3106ff
DAngelo <dangelo.watson1212@gmail.com>
2025-05-17 19:48:56 -0400

    Updates

---
+++ precache-manifest.dc1be8fa3cb2cb931b1bb450bd3befc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2ed2d8b14f3fdd471234129aeadd40",
+    "url": "/spotify-api/index.html"
+  },
+  {
+    "revision": "b0fea437ba57ac64b272",
+    "url": "/spotify-api/static/css/main.b071627b.chunk.css"
+  },
+  {
+    "revision": "04268bec09f90435dc01",
+    "url": "/spotify-api/static/js/2.d5da4b02.chunk.js"
+  },
+  {
+    "revision": "b0fea437ba57ac64b272",
+    "url": "/spotify-api/static/js/main.812b846a.chunk.js"
+  },
+  {
+    "revision": "b5fe56623f5155e5ac1f",
+    "url": "/spotify-api/static/js/runtime~main.f36fa6bf.js"
+  },
+  {
+    "revision": "1e03fc80308fec0118f90235875763ba",
+    "url": "/spotify-api/static/media/cover.1e03fc80.jpg"
+  }
+]);
